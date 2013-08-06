f92804057ab0e7583ae04ccf2b7aa64b591db580
leonardowolter <leocwolter@gmail.com>
2013-08-06 17:26:37 -0300

    verifying if method returns boolean, testing and better exception message

---
+++ src/test/java/br/com/caelum/brutauth/reflection/methodsearchers/DefaultMethodSearcherTest.java
@@ -0,0 +1,70 @@
+package br.com.caelum.brutauth.reflection.methodsearchers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Method;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.brutauth.auth.rules.CustomBrutauthRule;
+import br.com.caelum.brutauth.reflection.BrutauthValidation;
+
+public class DefaultMethodSearcherTest {
+
+	private DefaultMethodSearcher methodSearcher;
+
+	@Before
+	public void setUp(){
+		methodSearcher = new DefaultMethodSearcher();
+	}
+	
+	
+	@Test
+	public void should_return_is_allowed_method() throws NoSuchMethodException {
+		Method method = methodSearcher.getMethod(new MyCustomRuleWithIsAllowedMethod());
+		assertEquals("isAllowed", method.getName());
+	}
+	
+	@Test
+	public void should_return_annotated_method() throws NoSuchMethodException {
+		Method method = methodSearcher.getMethod(new MyCustomRuleWithAnnotatedMethod());
+		assertTrue("should contains @" + BrutauthValidation.class.getSimpleName() + " annnotation", method.isAnnotationPresent(BrutauthValidation.class));
+	}
+	
+	@Test(expected = NoSuchMethodException.class)
+	public void should_not_return_is_allowed_method_if_it_doesnt_returns_boolean() throws NoSuchMethodException {
+		methodSearcher.getMethod(new MyCustomRuleWithInvalidIsAllowedMethod());
+	}
+	
+	@Test(expected = NoSuchMethodException.class)
+	public void should_not_return_annotated_method_if_it_doesnt_returns_boolean() throws NoSuchMethodException {
+		methodSearcher.getMethod(new MyCustomRuleWithInvalidAnnotatedMethod());
+	}
+	
+	private class MyCustomRuleWithIsAllowedMethod implements CustomBrutauthRule{
+		public boolean isAllowed() {
+			return false;
+		}
+	}
+	
+	private class MyCustomRuleWithInvalidIsAllowedMethod implements CustomBrutauthRule{
+		public void isAllowed() {
+		}
+	}
+	
+	private class MyCustomRuleWithAnnotatedMethod implements CustomBrutauthRule{
+		@BrutauthValidation
+		public boolean annotherName() {
+			return false;
+		}
+	}
+	
+	private class MyCustomRuleWithInvalidAnnotatedMethod implements CustomBrutauthRule{
+		@BrutauthValidation
+		public void annotherName() {
+		}
+	}
+
+}
